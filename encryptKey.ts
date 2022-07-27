b00ae3eeadd0eb195e0126290d33f1e61c9c1992
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-07-27 14:33:56 -0600

    updated RPC, encrypted password, and added Typescript files for later

---
+++ encryptKey.ts
@@ -0,0 +1,22 @@
+import { ethers } from "ethers"
+import * as fs from "fs"
+import "dotenv/config"
+
+async function main() {
+	const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!)
+	//this will encrypt the private key. only need it in .env once to run, then needs password to decrypt
+	//input needs PWD and KEY
+	const encryptedJsonKey = await wallet.encrypt(
+		process.env.KEY_PASSWORD!,
+		process.env.PRIVATE_KEY!
+	)
+	//saves the encrypted JSON key to a new file
+	fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error)
+		process.exit(1)
+	})
